b7c3d422da8f6af9e4f15e207a3a82e22395c164
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2018-11-21 20:39:02 +0200

    break continue

---
+++ continue_break.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if ( number < 0 )
+                {
+                    continue; // continue to the next for 
+                }
+
+                if ( number == 0 )
+                {
+                    break; // get out of the for [terminate]
+                }
+
+                sum = sum + number;
+            }
+
+            Console.WriteLine( sum );
+
+        }
+    }
+}

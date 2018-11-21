38058fc10b107a55d5d02f71b47d7e38f2b3a4c1
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2018-11-21 20:54:56 +0200

    Targil 4 stars

---
+++ hw_4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stars = Convert.ToInt32(Console.ReadLine()); // read a number
+
+            int i = 1; // number of starts i am drawing in the current row
+
+            int spaces = stars / 2; // number of spaces in each row
+
+            while (i <= stars)
+            {
+                // draw the spaces
+                for (int j = 1; j <= spaces; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                // draw the stars
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+
+                // add 2 to i
+                i = i + 2;
+
+                // decrease 1 to spaces
+                spaces = spaces - 1;
+            }
+        }
+    }
+}

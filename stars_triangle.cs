98baef9f8559d59c3fa44839e12d33d1833505a4
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2018-11-21 21:05:47 +0200

    Stars triangle

---
+++ stars_triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2111_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = Convert.ToInt32(Console.ReadLine()); // read a number
+
+            // run from 1 -> rows
+            for (int i = 1; i <= rows; i++)
+            {
+
+                // print all numbers from 1 to i
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j); // print j do not line feed
+                }
+
+                Console.WriteLine(); // line feed
+            }
+
+            // run from rows -> 1
+            for (int i = rows - 1; i >= 1; i--)
+            {
+
+                // print all numbers from 1 to i
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j); // print j do not line feed
+                }
+
+                Console.WriteLine(); // line feed
+            }
+        }
+    }
+}
